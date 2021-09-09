ca80e3aebb862a78457defdb87a534b0af040cb9
DanyaRubin <masterovski@gmail.com>
2021-09-09 16:50:45 +0400

    08091

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,41 +1,41 @@
 package com.company;
+
 import java.util.Scanner;
 
-import static com.company.NumberConvert.transform_number_to_roman_numeral;
 import static java.lang.Integer.parseInt;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Давайте посчитаем (для завершения введите : exit)");
-        Scanner sc = new Scanner(System.in);
+public static void main(String[] args) {
+    System.out.println("Давайте посчитаем (для завершения введите : exit)");
+    Scanner sc = new Scanner(System.in);
 
-        while (true) {
+    while (true) {
         String str = sc.nextLine();
-            if (str.equals("exit")) {
-                System.out.println("Bye...");
-                break;
-            }
-
-        String[] result = str.split(" ");
-            int a = parseInt(NumberConvert.checkRome(result[0]));
-            int b = parseInt (NumberConvert.checkRome(result[2]));
-        String operator = result[1];
-        if (a == 0 || a > 10 || b == 0 || b > 10){
-            System.out.println("Ошибка, выбран не верный диапазон");
+        if (str.equals("exit")) {
             System.out.println("Bye...");
             break;
-
-        } else if (operator.contains("-")){
-            System.out.println (transform_number_to_roman_numeral(a - b));
-        } else if (operator.contains("*")){
-            System.out.println (transform_number_to_roman_numeral(a * b));
-        } else if (operator.contains("/")){
-            System.out.println(transform_number_to_roman_numeral(a / b));
-        } else if (operator.contains("+")){
-            System.out.println(transform_number_to_roman_numeral(a + b));
         }
-        }
+        NumberConvert.stopRome(str);
+        String[] result = str.split(" ");
 
+
+        int a = parseInt(NumberConvert.checkRome(result[0]));
+        int b = parseInt(NumberConvert.checkRome(result[2]));
+        String operator = result[1];
+
+        if (str.equals("V")) {
+            NumberConvert.finalConvertRome(a, b, operator);
+        } else if (str.equals("I")) {
+            NumberConvert.finalConvertRome(a, b, operator);
+        } else if (str.equals("X")) {
+            NumberConvert.finalConvertRome(a, b, operator);
+        } else {
+            NumberConvert.finalConvertArab(a, b, operator);
+        }
     }
 
 }
+
+}
+
 
--- src/com/company/NumberConvert.java
+++ src/com/company/NumberConvert.java
@@ -5,46 +5,99 @@ import java.util.ArrayList;
 public class NumberConvert {
 
 
-    public static String transform_number_to_roman_numeral(int number) {
-        int[] roman_value_list = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] roman_char_list = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        StringBuilder res = new StringBuilder();
-        for (int i = 0; i < roman_value_list.length; i += 1) {
-            while (number >= roman_value_list[i]) {
-                number -= roman_value_list[i];
-                res.append(roman_char_list[i]);
-            }
+public static String transform_number_to_roman_numeral(int number) {
+    int[] roman_value_list = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    String[] roman_char_list = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    StringBuilder res = new StringBuilder();
+    for (int i = 0; i < roman_value_list.length; i += 1) {
+        while (number >= roman_value_list[i]) {
+            number -= roman_value_list[i];
+            res.append(roman_char_list[i]);
         }
-        return res.toString();
     }
+    return res.toString();
+}
 
 
-    public static int romeToArab(String str) {
-        ArrayList<String> rome = new ArrayList<>();
-        rome.add("I");
-        rome.add("II");
-        rome.add("III");
-        rome.add("IV");
-        rome.add("V");
-        rome.add("VI");
-        rome.add("VII");
-        rome.add("VIII");
-        rome.add("IX");
-        rome.add("X");
-        return rome.indexOf(str);
+public static int romeToArab(String str) {
+    ArrayList<String> rome = new ArrayList<>();
+    rome.add("I");
+    rome.add("II");
+    rome.add("III");
+    rome.add("IV");
+    rome.add("V");
+    rome.add("VI");
+    rome.add("VII");
+    rome.add("VIII");
+    rome.add("IX");
+    rome.add("X");
+    return rome.indexOf(str);
 
-    }
+}
 
-    public static String checkRome(String str) {
-        if (str.contains("V")) {
-            return String.valueOf(romeToArab(str) + 1);
-        } else if (str.contains("I")) {
-            return String.valueOf(romeToArab(str) + 1);
-        } else if (str.contains("X")) {
-            return String.valueOf(romeToArab(str) + 1);
-        }
+public static String checkRome(String str) {
+    if (str.contains("V")) {
+        return String.valueOf(romeToArab(str) + 1);
+    } else if (str.contains("I")) {
+        return String.valueOf(romeToArab(str) + 1);
+    } else if (str.contains("X")) {
+        return String.valueOf(romeToArab(str) + 1);
+    } else
         return str;
+}
+
+public static void finalConvertArab(int a, int b, String str) {
+    if (a == 0 || a > 10 || b == 0 || b > 10) {
+        System.out.println("Ошибка, выбран не верный диапазон");
+        System.out.println("Bye...");
+
+    } else if (str.contains("-")) {
+        System.out.println(a - b);
+    } else if (str.contains("*")) {
+        System.out.println(a * b);
+    } else if (str.contains(" / ")) {
+        System.out.println(a / b);
+    } else if (str.contains("+")) {
+        System.out.println(a + b);
     }
+}
+
+public static void finalConvertRome(int a, int b, String str) {
+    if (a == 0 || a > 10 || b == 0 || b > 10) {
+        System.out.println("Ошибка, выбран не верный диапазон");
+        System.out.println("Bye...");
+    } else if (str.contains("-")) {
+        System.out.println(transform_number_to_roman_numeral(a - b));
+    } else if (str.contains("*")) {
+        System.out.println(transform_number_to_roman_numeral(a * b));
+    } else if (str.contains(" / ")) {
+        System.out.println(transform_number_to_roman_numeral(a / b));
+    } else if (str.contains("+")) {
+        System.out.println(transform_number_to_roman_numeral(a + b));
+    }
+}
+
+public static void stopRome(String str) {
+    while (true) if (str.contains("C")) {
+        System.out.println("Ошибка, выбран не верный диапазон");
+        System.out.println("Bye...");
+        break;
+    } else if (str.contains("L")) {
+        System.out.println("Ошибка, выбран не верный диапазон");
+        System.out.println("Bye...");
+        break;
+    } else if (str.contains("D")) {
+        System.out.println("Ошибка, выбран не верный диапазон");
+        System.out.println("Bye...");
+        break;
+    } else if (str.contains("M")) {
+        System.out.println("Ошибка, выбран не верный диапазон");
+        System.out.println("Bye...");
+        break;
+    } else {
+        return;
+    }
+}
 }
 
 
